7bd72d453d1d3c1168c404b231438736d2bb11c0
Margarida Paixao <margarida.paixao@freitag.de>
2023-10-03 11:36:57 +0200

    volto to find an ipv4 address for the backend first

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,10 @@
  * Replace with custom runner when needed.
  * @module index
  */
-
+import dns from 'dns';
 import start from '@plone/volto/start-server';
 
+dns.setDefaultResultOrder('ipv4first');
 const reloadServer = start();
 
 if (module.hot) {
